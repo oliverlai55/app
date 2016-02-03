70f24b05e4d80410803f18021cb3d07ea46842a6
Oliver Lai <oliverlai55@gmail.com>
2016-02-03 18:26:56 -0500

    added modules

--- js/7MinWorkout/app.js
+++ js/7MinWorkout/app.js
@@ -1,3 +1,3 @@
 angular.module('app',[]);
-
+//creates new modules
 angular.module('7minWorkout',[]);
--- js/7MinWorkout/workout.js
+++ js/7MinWorkout/workout.js
@@ -1,1 +1,10 @@
-workout.js
+angular.module('7minWorkout').controller('WorkoutController', function($scope){
+//gets existing module
+
+	var restExercise;
+	var workoutPlan;
+	var init = function(){
+		startWokrout();
+	};
+	init();
+});
