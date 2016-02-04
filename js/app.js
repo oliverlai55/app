61a93d77e75a53cd1f7905a81a8ff98582fd6195
Oliver Lai <oliverlai55@gmail.com>
2016-02-04 16:54:46 -0500

    adding different pages

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-angular.module('app',['ngRoute', '7minWorkout']);
+'use strict';
+
+angular.module('app',['ngRoute', '7minWorkout']).
 
 config(function ($routeProvider){
 	$routeProvider.when('/start', {
 		templateUrl: 'partials/start.html'
 	});
-	$routeProvider.when('/workout' {
-		templateUrl: 'partials/workout.html'
+	$routeProvider.when('/workout', {
+		templateUrl: 'partials/workout.html',
 		controller: 'WorkoutController'
 	});
 	$routeProvider.when('/finish', {
