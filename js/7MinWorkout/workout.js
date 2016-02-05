4272cf8f33a82b730e7f6991a4847816b7bdd461
Oliver Lai <oliverlai55@gmail.com>
2016-02-05 10:56:20 -0500

    updating workout.html

--- js/7MinWorkout/workout.js
+++ js/7MinWorkout/workout.js
@@ -34,10 +34,10 @@ angular.module('7minWorkout')
       }
 
       var restExercise;
-      var workoutPlan;
+
       var startWorkout = function () {
-          workoutPlan = createWorkout();
-          $scope.workoutTimeRemaining = workoutPlan.totalWorkoutDuration();
+          $scope.workoutPlan = createWorkout();
+          $scope.workoutTimeRemaining = $scope.workoutPlan.totalWorkoutDuration();
 
           restExercise = {
               details: new Exercise({
@@ -46,14 +46,14 @@ angular.module('7minWorkout')
                   description: "Relax a bit!",
                   image: "img/rest.png",
               }),
-              duration: workoutPlan.restBetweenExercise
+              duration: $scope.workoutPlan.restBetweenExercise
           };
 
           $interval(function(){
             $scope.workoutTimeRemaining = $scope.workoutTimeRemaining - 1;
           }, 1000, $scope.workoutTimeRemaining);
           
-          startExercise(workoutPlan.exercises.shift());
+          startExercise($scope.workoutPlan.exercises.shift());
       };
 
       var startExercise = function (exercisePlan) {
@@ -75,10 +75,10 @@ angular.module('7minWorkout')
       var getNextExercise = function (currentExercisePlan) {
           var nextExercise = null;
           if (currentExercisePlan === restExercise) {
-              nextExercise = workoutPlan.exercises.shift();
+              nextExercise = $scope.workoutPlan.exercises.shift();
           }
           else {
-              if (workoutPlan.exercises.length != 0) {
+              if ($scope.workoutPlan.exercises.length != 0) {
                   nextExercise = restExercise;
               }
           }
