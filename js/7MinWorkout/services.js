20a5bebb1a0c7612b1896f03b1c05a1833adab8e
Oliver Lai <oliverlai55@gmail.com>
2016-02-07 17:32:32 -0500

    adding more modules

--- js/7MinWorkout/services.js
+++ js/7MinWorkout/services.js
@@ -1,1 +1,9 @@
-services.js
+angular.module('7minWorkout')
+	.factory('workoutHistoryTracker', ['$rootScope', 
+		function($rootScope){
+			var maxHistoryItems = 20;
+			var workoutHistory = [];
+			var currentWorkoutLog = null;
+			var service = {};
+			return service;
+	}]);
